ee5c7e6ef1132f32358e1589e97ab13982a97aab
Maxime Ancelin <ancelin.maxime@gmail.com>
2013-12-01 20:30:30 +0100

    Use var for title.

--- mancelin.py
+++ mancelin.py
@@ -17,19 +17,19 @@ def index():
 
 @app.route("/CV")
 def cv():
-    return render_template('CV.html')
+    return render_template('CV.html', title=' - CV')
 
 @app.route("/projets")
 def projets():
-    return render_template('projets.html')
+    return render_template('projets.html', title=' - Projets')
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title=' - Contact')
 
 @app.route("/sites_amis")
 def sites_amis():
-    return render_template('sites_amis.html')
+    return render_template('sites_amis.html', title=' - Site amis')
 
 
 if __name__ == "__main__":
