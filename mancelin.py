4d052e56b79816ece98dffcca510ffb4aa66a5fd
Maxime Ancelin <ancelin.maxime@gmail.com>
2013-12-01 18:07:00 +0100

    Base site using bootstrap.

--- mancelin.py
+++ mancelin.py
@@ -15,5 +15,22 @@ app.config.from_pyfile('./config.ini', silent=True)
 def index():
     return render_template('index.html')
 
+@app.route("/CV")
+def cv():
+    return render_template('CV.html')
+
+@app.route("/projets")
+def projets():
+    return render_template('projets.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/sites_amis")
+def sites_amis():
+    return render_template('sites_amis.html')
+
+
 if __name__ == "__main__":
     app.run()
