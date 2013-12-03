a2c092aa8b814be02bb4c14ce587a61bded7cfe7
Maxime Ancelin <ancelin.maxime@gmail.com>
2013-12-03 14:26:34 +0100

    Use MongoDb.

--- mancelin.py
+++ mancelin.py
@@ -2,18 +2,35 @@
 
 
 from flask import Flask, render_template
+import pymongo
 
 
-# configuration
-DEBUG = True
+# configuration db
+db_host_name = 'localhost'
+db_port = 27017
+db_name = 'mancelin'
 
 # create application
 app = Flask(__name__)
 app.config.from_pyfile('./config.ini', silent=True)
+db = pymongo.Connection(host=db_host_name, port=db_port)[db_name]
 
+
+# routes
 @app.route("/")
 def index():
     return render_template('index.html')
+
+#@app.route("/add_project")
+@app.route("/add")
+def add_project():
+    db.projets.save( { 'name' : "mongo" } , safe=True)
+    print "add !"
+    return render_template('index.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title=' - Contact')
 
 @app.route("/CV")
 def cv():
@@ -23,9 +40,6 @@ def cv():
 def projets():
     return render_template('projets.html', title=' - Projets')
 
-@app.route("/contact")
-def contact():
-    return render_template('contact.html', title=' - Contact')
 
 @app.route("/sites_amis")
 def sites_amis():
